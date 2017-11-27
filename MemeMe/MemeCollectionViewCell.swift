416b541271fc7e12a3fa4d65f407c0ea211f4ae4
Alec O’Connor <Alec@AlecOConnor.com>
2017-11-27 00:17:24 -0500

    Updated app with table and collection view controllers

---
+++ MemeMe/MemeCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  MemeCollectionViewCell.swift
+//  MemeMe
+//
+//  Created by Alec O'Connor on 11/26/17.
+//  Copyright © 2017 Alec O'Connor. All rights reserved.
+//
+
+import UIKit
+
+class MemeCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ MemeMe/MemeTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MemeTableViewCell.swift
+//  MemeMe
+//
+//  Created by Alec O'Connor on 11/26/17.
+//  Copyright © 2017 Alec O'Connor. All rights reserved.
+//
+
+import UIKit
+
+class MemeTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
